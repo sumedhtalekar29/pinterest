ae869df47503581bf1205923be9deb606e54dcec
sumedhtalekar29 <sumedh.talekar@gmail.com>
2020-09-03 12:36:33 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+  // private :: using session
+  { path: 'home', component: HomeComponent },
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
