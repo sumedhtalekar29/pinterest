b070a35e63f273c057801c51ba8861744b377f96
sumedhtalekar29 <sumedh.talekar@gmail.com>
2020-09-04 16:33:04 +0530

    updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  urls=[];
+  onselect(e){
+    if(e.target.files){
+      for(let i=0; i<File.length; i++)
+      {
+        var reader= new FileReader();
+        reader.readAsDataURL(e.target.files[i]);
+        reader.onload=(events:any) =>{
+          this.urls.push(events.target.result);
+        }
+      }
+    }
   }
 
 }
